5a0daff6431245abdd7e1996b4201db5bd0b592e
carbonarangluis <luis.hwtg@gmail.com>
2023-11-01 22:49:33 +0800

    feat select active players syntax

--- hero.sql
+++ hero.sql
@@ -165,3 +165,8 @@ VALUES
     UPDATE public.hero
     SET is_active = false
     WHERE hero_id = 1;
+
+    SELECT p.player_name, h.hero_name
+    FROM public.player p
+    JOIN public.hero h ON p.hero_id = h.hero_id
+    WHERE h.is_active = true;
