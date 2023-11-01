497c6d3c9473fecea5096f57cc6fcc5d8e05a8ec
carbonarangluis <luis.hwtg@gmail.com>
2023-11-01 22:54:15 +0800

    feat select heroes archers syntax

--- hero.sql
+++ hero.sql
@@ -170,3 +170,8 @@ VALUES
     FROM public.player p
     JOIN public.hero h ON p.hero_id = h.hero_id
     WHERE h.is_active = true;
+
+    SELECT h.hero_name
+    FROM public.hero h
+    JOIN public.class c ON h.class_id = c.class_id
+    WHERE c.class_name = 'Archer';
